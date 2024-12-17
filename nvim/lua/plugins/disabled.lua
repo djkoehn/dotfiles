d33d6420f1c8ba38d1b4bfa72ad4f28af3bf3b15
djkoehn <djkoehn2@outlook.com>
2024-12-17 14:00:29 -0600

    Delete nvim directory

--- nvim/lua/plugins/disabled.lua
+++
@@ -1,8 +0,0 @@
-return {
-  { "hrsh8th/nvim-cmp",             enabled = false },
-  { "L3MON4D3/LuaSnip",             enabled = false },
-  { "saghen/blink.cmp",             enabled = false },
-  { "rafamadriz/friendly-snippets", enabled = false },
-  { "folke/todo-comments.nvim",     enabled = false },
-  { "folke/ts-comments.nvim",       enabled = false }
-}

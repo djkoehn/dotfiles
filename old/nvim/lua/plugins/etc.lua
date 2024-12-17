d30eaece7bedd08ed073ed45c72d0eec1c62bf94
djkoehn <djkoehn2@outlook.com>
2024-12-17 14:04:12 -0600

    Add files via upload

---
+++ old/nvim/lua/plugins/etc.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "junegunn/goyo.vim",
+  },
+
+  {
+    "kevinhwang91/nvim-ufo",
+    "kevinhwang91/promise-async",
+  },
+
+  {
+    "xiyaowong/transparent.nvim",
+  },
+
+  {
+    "norcalli/nvim-colorizer.lua",
+  },
+
+  {
+    "iamcco/markdown-preview.nvim",
+  },
+}

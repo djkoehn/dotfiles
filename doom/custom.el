5b8c0d9f1c4350b3aa2ac3143381f4abaf94cfe4
djkoehn <djkoehn2@outlook.com>
2024-12-19 03:08:44 -0600

    Add files via upload

---
+++ doom/custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files '("~/Documents/work/work.org"))
+ '(package-selected-packages '(nerd-icons peep-dired w3m ranger)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
